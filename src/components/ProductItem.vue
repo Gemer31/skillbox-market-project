f0063bd1b041e602c082a02b62d711e0a4104280
rakr0819 <raman.krauchanka@netcracker.com>
2022-08-14 11:58:58 +0300

    Skillbox vue start

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <a class="catalog__pic" href="#">
+    <img :src="product.imageSrc" alt="Название товара">
+  </a>
+
+  <h3 class="catalog__title">
+    <a href="#">
+      {{product.name}}
+    </a>
+  </h3>
+
+  <span class="catalog__price">
+              {{product.price}}
+            </span>
+
+  <!--        <ul class="colors colors&#45;&#45;black">-->
+  <!--          <li class="colors__item">-->
+  <!--            <label class="colors__label">-->
+  <!--              <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">-->
+  <!--              <span class="colors__value" style="background-color: #73B6EA;">-->
+  <!--                  </span>-->
+  <!--            </label>-->
+  <!--          </li>-->
+  <!--          <li class="colors__item">-->
+  <!--            <label class="colors__label">-->
+  <!--              <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">-->
+  <!--              <span class="colors__value" style="background-color: #8BE000;">-->
+  <!--                  </span>-->
+  <!--            </label>-->
+  <!--          </li>-->
+  <!--          <li class="colors__item">-->
+  <!--            <label class="colors__label">-->
+  <!--              <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">-->
+  <!--              <span class="colors__value" style="background-color: #222;">-->
+  <!--                  </span>-->
+  <!--            </label>-->
+  <!--          </li>-->
+  <!--        </ul>-->
+</template>
+
+<script>
+export default {
+  name: 'ProductItem',
+  props: [
+    'product',
+  ],
+};
+</script>
