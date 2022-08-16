c6027de122a5bb7ed9ccf649130d4fada8642ec8
rakr0819 <raman.krauchanka@netcracker.com>
2022-08-17 00:00:28 +0300

    Skillbox vue module 4

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,72 @@
 <template>
-  <section class="catalog">
-    <ProductList :products="products"/>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
 
-    <PaginationBar
-      v-model:current-page="currentPage"
-      :items-count="countProducts"
-      :items-per-page="productsPerPage"
-    />
-  </section>
+    <ProductFilter :price-from="filterPriceFrom"/>
+
+    <div class="content__catalog">
+      <section class="catalog">
+        <ProductList :products="products"/>
+
+        <PaginationBar
+          v-model:current-page="currentPage"
+          :items-count="countProducts"
+          :items-per-page="productsPerPage"
+        />
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
 import products from '@/data/products';
 import ProductList from '@/components/ProductList.vue';
 import PaginationBar from '@/components/PaginationBar.vue';
+import ProductFilter from '@/components/ProductFilter.vue';
 
 export default {
   name: 'App',
   components: {
-    ProductList, PaginationBar,
+    ProductList,
+    PaginationBar,
+    ProductFilter,
   },
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 'bikes',
       currentPage: 1,
       productsPerPage: 3,
     };
   },
   computed: {
+    filteredProducts() {
+      let filteredProducts = products();
+      if (this.filterPriceFrom > 0) {
+        filteredProducts = filteredProducts.filter((product) => product.price > this.filterPriceFrom);
+      }
+      if (this.filterPriceTo > 0) {
+        filteredProducts = filteredProducts.filter((product) => product.price < this.filterPriceTo);
+      }
+      if (this.filterCategoryId) {
+        filteredProducts = filteredProducts.filter((product) => product.categoryId === this.filterCategoryId);
+      }
+      return filteredProducts;
+    },
     products() {
       const offset = (this.currentPage - 1) * this.productsPerPage;
-      return products().slice(offset, offset + this.productsPerPage);
+      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
     },
     countProducts() {
-      return products().length;
+      return this.filteredProducts.length;
     },
   },
 };
---
+++ src/data/categories.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 'bikes',
+    title: 'Велосипеды',
+  },
+  {
+    id: 'chairs',
+    title: 'Компьютерные стулья',
+  },
+  {
+    id: 'dishwashers',
+    title: 'Посудомоечные машины',
+  },
+];
