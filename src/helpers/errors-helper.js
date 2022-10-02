79b5c09b64d5d9a5d0e634cff67e99959d4c31be
rakr0819 <raman.krauchanka@netcracker.com>
2022-10-02 22:48:31 +0300

    TZ

--- src/helpers/errors-helper.js
+++ src/helpers/errors-helper.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export function parseErrorMessage(error) {
-  return error.response?.data?.error?.message || Object.values(error.response?.data?.error?.request)?.[0];
+  return error.response?.data?.error?.message
+    || Object.values(error.response?.data?.error?.query || error.response?.data?.error?.request)?.[0];
 }
